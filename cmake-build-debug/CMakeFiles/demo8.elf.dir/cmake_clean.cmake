35361cd1f8007afb99414caf92bf763de3ade447
chenre-cjx <jiaxing.c919@gmial.com>
2021-10-07 14:39:12 +0800

    git init

---
+++ cmake-build-debug/CMakeFiles/demo8.elf.dir/cmake_clean.cmake
@@ -0,0 +1,34 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/demo8.elf.dir/Core/Src/gpio.c.obj"
+  "CMakeFiles/demo8.elf.dir/Core/Src/main.c.obj"
+  "CMakeFiles/demo8.elf.dir/Core/Src/stm32f4xx_hal_msp.c.obj"
+  "CMakeFiles/demo8.elf.dir/Core/Src/stm32f4xx_it.c.obj"
+  "CMakeFiles/demo8.elf.dir/Core/Src/syscalls.c.obj"
+  "CMakeFiles/demo8.elf.dir/Core/Src/system_stm32f4xx.c.obj"
+  "CMakeFiles/demo8.elf.dir/Core/Src/usart.c.obj"
+  "CMakeFiles/demo8.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal.c.obj"
+  "CMakeFiles/demo8.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_cortex.c.obj"
+  "CMakeFiles/demo8.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma.c.obj"
+  "CMakeFiles/demo8.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma_ex.c.obj"
+  "CMakeFiles/demo8.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_exti.c.obj"
+  "CMakeFiles/demo8.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash.c.obj"
+  "CMakeFiles/demo8.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ex.c.obj"
+  "CMakeFiles/demo8.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ramfunc.c.obj"
+  "CMakeFiles/demo8.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_gpio.c.obj"
+  "CMakeFiles/demo8.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr.c.obj"
+  "CMakeFiles/demo8.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr_ex.c.obj"
+  "CMakeFiles/demo8.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc.c.obj"
+  "CMakeFiles/demo8.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc_ex.c.obj"
+  "CMakeFiles/demo8.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim.c.obj"
+  "CMakeFiles/demo8.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim_ex.c.obj"
+  "CMakeFiles/demo8.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_uart.c.obj"
+  "CMakeFiles/demo8.elf.dir/startup/startup_stm32f407xx.s.obj"
+  "demo8.elf"
+  "demo8.elf.manifest"
+  "demo8.elf.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang ASM C)
+  include(CMakeFiles/demo8.elf.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
